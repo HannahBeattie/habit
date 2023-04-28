3f9772db7e62c24f948899fc8ed5404a3f41104e
Hannah Beattie <hannahdbeattie@gmail.com>
2023-04-28 17:18:04 +1200

    play w styling

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,7 @@
 import { Stack } from 'expo-router'
 import { StyleProvider } from '../theme/StyleProvider'
 import React, { useState, useEffect } from 'react'
+
 import {
 	NotoSansSC_100Thin as bodyThin,
 	NotoSansSC_300Light as bodyLight,
@@ -9,11 +10,25 @@ import {
 	NotoSansSC_700Bold as bodyBold,
 	NotoSansSC_900Black as bodyBlack,
 } from '@expo-google-fonts/noto-sans-sc'
+import {
+	Aleo_300Light as aLight,
+	Aleo_300Light_Italic as aItalic,
+	Aleo_400Regular as aRegular,
+	Aleo_400Regular_Italic as aRegItalic,
+	Aleo_700Bold as aBold,
+	Aleo_700Bold_Italic as aBoldItalic,
+} from '@expo-google-fonts/aleo'
 import { useFonts, Copse_400Regular as heading } from '@expo-google-fonts/copse'
 import AppLoading from 'expo-app-loading'
 function App() {
 	let [fontsLoaded] = useFonts({
 		heading,
+		aLight,
+		aItalic,
+		aRegular,
+		aRegItalic,
+		aBold,
+		aBoldItalic,
 		bodyThin,
 		body,
 		bodyBold,
--- app/index.tsx
+++ app/index.tsx
@@ -19,35 +19,45 @@ import { SafeAreaView } from 'react-native'
 
 export default function Home() {
 	const headingProps = {
-		fontSize: '6xl',
-		color: 'gray.900',
-		pb: 2,
+		fontSize: '5xl',
+		color: 'blueGray.700',
+		pr: 12,
+		fontFamily: 'aRegItalic',
+		pb: 8,
 	}
 
 	return (
 		<VStack flex={1} space={8} bg={'#dad0e2'}>
 			<SafeAreaView style={{ flex: 1 }}>
-				<VStack space={4} px={8} py={70} shadow={3} bg={'transparent'}>
+				<VStack px={8} py={20} bg={'transparent'}>
 					<Heading {...headingProps}>What should I call you?</Heading>
-					<Input
-						borderColor={'gray.200'}
-						color={'gray.900'}
-						borderWidth={3}
-						bgColor={'blueGray.100'}
-						p={2}
-						fontSize={'2xl'}
-						bg={'gray.100'}
-						variant='filled'
-						placeholder='hmmm ???'
-					/>
-					<HStack>
-						<Spacer />
-						<IconButton
-							bg={'gray.900'}
-							borderRadius={200}
-							icon={<CheckIcon color={'gray.200'} size={8} />}
+					<VStack space={6} shadow={4} bg={'transparent'}>
+						<Input
+							borderRadius={10}
+							borderColor={'gray.200'}
+							color={'gray.900'}
+							borderWidth={3}
+							bgColor={'blueGray.300'}
+							p={2}
+							fontSize={'2xl'}
+							bg={'gray.100'}
+							variant='filled'
+							placeholderTextColor={'blueGray.300'}
+							placeholder='hmmm ?'
 						/>
-					</HStack>
+
+						<HStack>
+							<Spacer />
+							<IconButton
+								borderColor={'gray.200'}
+								borderWidth={3}
+								shadow={6}
+								bg={'gray.900'}
+								borderRadius={200}
+								icon={<CheckIcon color={'gray.200'} size={8} />}
+							/>
+						</HStack>
+					</VStack>
 				</VStack>
 			</SafeAreaView>
 		</VStack>
