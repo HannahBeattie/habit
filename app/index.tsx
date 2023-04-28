e9950a3a7eb87faf6f2aa4ab1320ed2fef7cf9e0
Hannah Beattie <hannahdbeattie@gmail.com>
2023-04-28 19:00:26 +1200

    create logo

--- app/index.tsx
+++ app/index.tsx
@@ -1,4 +1,6 @@
 import {
+	Box,
+	Center,
 	CheckIcon,
 	CircleIcon,
 	HStack,
@@ -19,8 +21,8 @@ import {
 import React from 'react'
 import { SafeAreaView, useWindowDimensions } from 'react-native'
 
+const logo = require('../assets/66.png')
 const image = require('../assets/grass.png')
-
 export default function Home() {
 	const { width } = useWindowDimensions()
 	const headingProps = {
@@ -32,61 +34,64 @@ export default function Home() {
 	}
 
 	return (
-		<VStack flex={1} space={8} bg={'#f3f0f4'}>
+		<VStack flex={1} space={8} bg={'#e7e4e8'}>
 			<SafeAreaView style={{ flex: 1 }}>
-				<HStack px={4} py={2}>
-					<VStack>
-						<Text fontSize={'xl'} fontFamily={'heading'} color={'gray.600'}>
-							66 days
-						</Text>
-						<Text fontSize={'xs'} fontFamily={'body'} color={'gray.600'}>
-							To build a habit
-						</Text>
-						<Spacer />
-					</VStack>
-				</HStack>
-				<VStack px={8} flex={1} py={16}>
-					<Heading {...headingProps}>What should I call you?</Heading>
-					<VStack space={6} shadow={3} bg={'transparent'}>
-						<VStack shadow={2} bgColor={'blueGray.200'} p={1} borderRadius={10}>
-							<VStack shadow={0}>
-								<Input
-									borderRadius={20}
-									borderColor={'gray.200'}
-									color={'gray.900'}
-									borderWidth={2}
-									shadow={4}
-									bgColor={'gray.200'}
-									p={2}
-									fontSize={'2xl'}
-									bg={'gray.100'}
-									variant='filled'
-									placeholderTextColor={'gray.400'}
-									placeholder='hmmm ?'
-								/>
-							</VStack>
+				<VStack flex={1} space={8}>
+					<SafeAreaView style={{ flex: 1 }}>
+						<VStack
+							shadow={8}
+							p={4}
+							bg={'gray.800'}
+							w={90}
+							h={90}
+							borderRightRadius={200}
+							borderColor={'gray.300'}
+							borderWidth={2}
+							borderLeftWidth={0}
+						>
+							<Heading fontSize={'lg'} fontFamily={'heading'} color={'gray.300'}>
+								66
+							</Heading>
+							<Heading fontFamily={'heading'} fontSize={'lg'} color={'gray.300'}>
+								days
+							</Heading>
 						</VStack>
 
-						<HStack>
-							<Spacer />
-							<IconButton
-								borderColor={'gray.100'}
-								borderWidth={3}
-								bg={'gray.200'}
-								borderRadius={200}
-								icon={<CheckIcon color={'gray.100'} size={8} />}
-							/>
-						</HStack>
-					</VStack>
-					<Image
-						maxH={300}
-						position={'absolute'}
-						bottom={-40}
-						opacity={50}
-						w={width}
-						alt={'watercolor grass'}
-						source={image}
-					/>
+						<VStack px={8} flex={1} py={20}>
+							<Heading {...headingProps}>What should I call you?</Heading>
+							<VStack space={6} shadow={3} bg={'transparent'}>
+								<VStack shadow={2} bgColor={'blueGray.200'} p={1} borderRadius={10}>
+									<VStack shadow={0}>
+										<Input
+											borderRadius={10}
+											borderColor={'gray.200'}
+											color={'gray.900'}
+											borderWidth={2}
+											shadow={4}
+											bgColor={'gray.200'}
+											p={2}
+											fontSize={'2xl'}
+											bg={'gray.100'}
+											variant='filled'
+											placeholderTextColor={'gray.400'}
+											placeholder='hmmm ?'
+										/>
+									</VStack>
+								</VStack>
+
+								<HStack>
+									<Spacer />
+									<IconButton
+										borderColor={'gray.100'}
+										borderWidth={3}
+										bg={'gray.200'}
+										borderRadius={200}
+										icon={<CheckIcon color={'gray.100'} size={8} />}
+									/>
+								</HStack>
+							</VStack>
+						</VStack>
+					</SafeAreaView>
 				</VStack>
 			</SafeAreaView>
 		</VStack>
